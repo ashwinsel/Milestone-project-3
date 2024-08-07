64adc4aedcd73755e587392d4bab22066e240ff4
Ashwinkarthik Selvaraj <b6rd31sv@students.codeinstitute.net>
2024-08-07 23:08:34 +0000

    Readme file updated

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import os
 from flask import (Flask, flash, render_template, 
     redirect, request, session, url_for, Response, 
     abort, jsonify)
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
@@ -12,11 +10,14 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
+
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
+
 mongo = PyMongo(app)
+
 
 @app.route("/")
 @app.route("/get_locations")
